1e1f68f2d08d691dd7c53335709c5faf193dac91
bartlomiej-gora <bartlomiej.gora@gmail.com>
2020-12-09 20:08:56 +0100

    Changed publisher to range

--- src/main/java/pl/bgora/reactive/server/PublisherController.java
+++ src/main/java/pl/bgora/reactive/server/PublisherController.java
@@ -5,18 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import java.time.Duration;
-import java.util.List;
-
-import static java.time.temporal.ChronoUnit.SECONDS;
-
 @RestController
 public class PublisherController {
 
     @GetMapping(value = "/data", produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<Integer> getData() {
-        List<Integer> list = List.of(1,2,3,4,5,6,7,8,9,10);
-        return Flux.fromIterable(list);
+        return Flux.range(1, 1024);
     }
 
 }
